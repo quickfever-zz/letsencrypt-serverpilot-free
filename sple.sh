47cc49921ae1ec7a32131eb9cc7c8244ebcb8fa2
Devendra Meena <idvndr@gmail.com>
2018-02-25 17:59:50 +0530

    Update sple.sh

--- sple.sh
+++ sple.sh
@@ -1,21 +1,54 @@
-#!/bin/bash
-#################################################
-#						#
-#	This script automates the installation	#
-#	of Let's Encrypt SSL certificates on	#
-#	your ServerPilot free plan		#
-#						#
-#################################################
+#!/bin/bash -   
+#title          :rwssl.sh
+#description    :A tiny script to automate the installation of Let's Encrypt SSL on ServerPilot servers.
+#author         :Rehmat Alam / Shared by quickfever
+#date           :20171108
+#version        :2.0.0
+#usage          :./rwssl.sh
+#notes          :       
+#bash_version   :3.2.57(1)-release
+#============================================================================
 
+if [[ $EUID -ne 0 ]]
+	then
+   echo -e "\e[33mYou aren't root. Permission issues may arise.\e[39m"
+fi
 
-theAction=$1
-domainName=$2
-appName=$3
+printf "What do you want to do with Let's Encrypt? (install/uninstall): " ; read -r theAction
+if [ -z "$theAction" ]
+	then
+	echo -e "\e[31mPlease specify the task. Should be either install or uninstall\e[39m"
+	exit
+else
+	if [[ ! $theAction =~ ^(install|uninstall)$ ]] 
+		then
+		echo -e "The task should be either \e[31minstall\e[39m or \e[31muninstall\e[39m"
+		exit
+	fi
+fi
+
+printf "Enter your domain name (Don't include www): " ; read -r domainName
+if [ -z "$domainName" ]
+	then
+	echo -e "\e[31mPlease provide the domain name\e[39m"
+	exit
+fi
+
+printf "Enter your ServerPilot app name: " ; read -r appName
+if [ -z "$appName" ]
+	then
+	echo -e "\e[31mPlease provide the app name\e[39m"
+	exit
+fi
+
+printf "Is this a main domain or sub-domain? (main/sub): " ; read -r domainType
+
 spAppRoot="/srv/users/serverpilot/apps/$appName"
-domainType=$4
 spSSLDir="/etc/nginx-sp/vhosts.d/"
+
 # Install Let's Encrypt libraries if not found
 if ! hash letsencrypt 2>/dev/null; then
+	echo -e "\e[33mLet's Encrypt libs not found. Installing the libraries....\e[39m"
 	lecheck=$(eval "apt-cache show letsencrypt 2>&1")
 	if [[ "$lecheck" == *"No"* ]]
 		then
@@ -27,28 +60,10 @@ if ! hash letsencrypt 2>/dev/null; then
 	fi
 fi
 
-if [ -z "$theAction" ]
-	then
-	echo -e "\e[31mPlease specify the task. Should be either install or uninstall\e[39m"
-	exit
-fi
-
-if [ -z "$domainName" ]
-	then
-	echo -e "\e[31mPlease provide the domain name\e[39m"
-	exit
-fi
-
 if [ ! -d "$spAppRoot" ]
 	then
 	echo -e "\e[31mThe app name seems invalid as we didn't find its directory on your server\e[39m"
 	exit 
-fi
-
-if [ -z "$appName" ]
-	then
-	echo -e "\e[31mPlease provide the app name\e[39m"
-	exit
 fi
 
 if [ "$theAction" == "uninstall" ]; then
@@ -60,27 +75,33 @@ elif [ "$theAction" == "install" ]; then
 		then
 		echo -e "\e[31mPlease provide the type of the domain (either main or sub)\e[39m"
 		exit
+	else
+		if [[ ! $domainType =~ ^(main|sub)$ ]]
+			then
+			echo -e "The domain type should be either \e[31main\e[39m or \e[31msub\e[39m"
+			exit
+		fi
 	fi
 	sudo service nginx-sp stop
-	echo -e "\e[32mChecks passed, press enter to continue\e[39m"
+	echo -e "\e[32mReady to install, press enter to continue\e[39m"
 	if [ "$domainType" == "main" ]; then
-		thecommand="letsencrypt certonly --register-unsafely-without-email --agree-tos -d $domainName -d www.$domainName"
+		thecommand="letsencrypt certonly  --standalone --register-unsafely-without-email --agree-tos -d $domainName -d www.$domainName"
 	elif [[ "$domainType" == "sub" ]]; then
-		thecommand="letsencrypt certonly --register-unsafely-without-email --agree-tos -d $domainName"
+		thecommand="letsencrypt certonly  --standalone --register-unsafely-without-email --agree-tos -d $domainName"
 	else
 		echo -e "\e[31mDomain type not provided. Should be either main or sub\e[39m"
 		exit
 	fi
 	output=$(eval $thecommand 2>&1) | xargs
-
+	
 	if [[ "$output" == *"too many requests"* ]]; then
 		echo "Let's Encrypt SSL limit reached. Please wait for a few days before obtaining more SSLs for $domainName"
 	elif [[ "$output" == *"Congratulations"* ]]; then
 	
 	if [ "$domainType" == "main" ]; then
 		sudo echo "server {
-	listen 443 ssl;
-	listen [::]:443 ssl;
+	listen 443 ssl http2;
+	listen [::]:443 ssl http2;
 	server_name
 	$domainName
 	www.$domainName
@@ -108,8 +129,8 @@ elif [ "$theAction" == "install" ]; then
 
 	elif [ "$domainType" == "sub" ]; then
 		sudo echo "server {
-	listen 443 ssl;
-	listen [::]:443 ssl;
+	listen 443 ssl http2;
+	listen [::]:443 ssl http2;
 	server_name
 	$domainName
 	;
@@ -134,45 +155,45 @@ elif [ "$theAction" == "install" ]; then
 	include /etc/nginx-sp/vhosts.d/$appName.d/*.conf;
 }" > "$spSSLDir$appName-ssl.conf"
 	fi
-
 		echo -e "\e[32mSSL should have been installed for $domainName with auto-renewal (via cron)\e[39m"
-
-		# Add a cron job for auto-ssl renewal
-		grep "sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
+		
 	elif [[ "$output" == *"Failed authorization procedure."* ]]; then
 		echo -e "\e[31m$domainName isn't being resolved to this server. Please check and update the DNS settings if necessary and try again when domain name points to this server\e[39m"
 	elif [[ ! $output ]]; then
-		# If no output, we will assume that a valid SSL already exists for this domain
-		# so we will just add the vhost
-		sudo echo "server {
-	listen 443 ssl;
-	listen [::]:443 ssl;
-	server_name
-	$domainName
-	www.$domainName
-	;
+		# If no output, we will check if SSL is already issued and if so
+		# we will just add the vhost
+		if [ -f "/etc/letsencrypt/live/$domainName/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/$domainName/privkey.pem" ]; then
+			sudo echo "server {
+		listen 443 ssl http2;
+		listen [::]:443 ssl http2;
+		server_name
+		$domainName
+		www.$domainName
+		;
 
-	ssl on;
+		ssl on;
 
-	ssl_certificate /etc/letsencrypt/live/$domainName/fullchain.pem;
-	ssl_certificate_key /etc/letsencrypt/live/$domainName/privkey.pem;
+		ssl_certificate /etc/letsencrypt/live/$domainName/fullchain.pem;
+		ssl_certificate_key /etc/letsencrypt/live/$domainName/privkey.pem;
 
-	root $spAppRoot/public;
+		root $spAppRoot/public;
 
-	access_log /srv/users/serverpilot/log/$appName/dev_nginx.access.log main;
-	error_log /srv/users/serverpilot/log/$appName/dev_nginx.error.log;
+		access_log /srv/users/serverpilot/log/$appName/dev_nginx.access.log main;
+		error_log /srv/users/serverpilot/log/$appName/dev_nginx.error.log;
 
-	proxy_set_header Host \$host;
-	proxy_set_header X-Real-IP \$remote_addr;
-	proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-	proxy_set_header X-Forwarded-SSL on;
-	proxy_set_header X-Forwarded-Proto \$scheme;
+		proxy_set_header Host \$host;
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		proxy_set_header X-Forwarded-SSL on;
+		proxy_set_header X-Forwarded-Proto \$scheme;
 
-	include /etc/nginx-sp/vhosts.d/$appName.d/*.nonssl_conf;
-	include /etc/nginx-sp/vhosts.d/$appName.d/*.conf;
-}" > "$spSSLDir$appName-ssl.conf"
-	echo -e "\e[32mSSL should have been installed for $domainName with auto-renewal (via cron)\e[39m"
-		grep "sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" /etc/crontab || sudo echo "@monthly sudo service nginx-sp stop && yes | letsencrypt renew &>/dev/null && service nginx-sp start && service nginx-sp reload" >> /etc/crontab
+		include /etc/nginx-sp/vhosts.d/$appName.d/*.nonssl_conf;
+		include /etc/nginx-sp/vhosts.d/$appName.d/*.conf;
+	}" > "$spSSLDir$appName-ssl.conf"
+		echo -e "\e[32mSSL should have been installed for $domainName with auto-renewal (via cron)\e[39m"
+		else
+			echo -e "\e[31mSSL cannot be obtained at the moment. Please try again.\e[39m"
+		fi
 	else
 		echo -e "\e[31mSomething unexpected occurred\e[39m"
 	fi 
